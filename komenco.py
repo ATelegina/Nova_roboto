e216d034b0d5fa48fbc7c1972a03af3f57033a1a
ATelegina <70072471+ATelegina@users.noreply.github.com>
2022-02-16 15:55:07 +0100

    Update komenco.py

--- komenco.py
+++ komenco.py
@@ -9,8 +9,10 @@ def play_sound():
 def CV2_stuff():
     # import your script B
     os.system('python main.py')
-
+def kloaku():
+    os.system('python kloaka_robot.py')
 
 
 Thread(target = play_sound).start() 
 Thread(target = CV2_stuff).start()
+Thread(target = kloaku).start()
